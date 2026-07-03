e3af7c99896e203f3fd9dd93144ec53a33174d46
panzhiwei <panzhiwei@kospet.com>
2026-07-03 17:35:07 +0800

    Remove ChatGPT article from writing list

--- js/main.js
+++ js/main.js
@@ -4,8 +4,7 @@
 const LINKS = {
   articles: {
     "01": "https://x.com/myzwilpan/status/2048965951035744410",
-    "02": "https://x.com/myzwilpan/article/2046791669778174339",
-    "03": "https://x.com/myzwilpan/article/2027635813597778149",
+    "02": "https://x.com/myzwilpan/article/2027635813597778149",
   },
   social: {
     github: "https://github.com/Allenlebron",
